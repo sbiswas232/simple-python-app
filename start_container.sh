9a1e0dbd063f7982b01918496ffbf588beb8c3ed
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2024-08-24 23:58:32 +0530

    Update and rename start_container.sh to scripts/start_container.sh

--- start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull sbiswas232/simple-python-flask-app:latest
-
-# Run the Docker image as a container
-docker run -d --name spf-cont -p 5000:5000 sbiswas232/simple-python-flask-app:latest
