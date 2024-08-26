a7f8cfe6e0733e8bac8e5c7a3db69592b6dd59e3
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2024-08-26 12:45:45 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+sudo docker pull sbiswas232/gitlab-webapp:latest
 
 # Run the Docker image as a container
-echo
+sudo docker run -d --rm --name gitlab-webapp-container -p 80:80 sbiswas232/gitlab-webapp:latest
