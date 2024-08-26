fb5b5496ffb72273d475bfebb2d311c58bc13c87
sbiswas232 <139542832+sbiswas232@users.noreply.github.com>
2024-08-26 13:55:03 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-sudo docker pull sbiswas232/gitlab-webapp:latest
+sudo docker pull sbiswas232/guard-webapp:adv1
+sudo docker pull sbiswas232/springboot-javaapp:v21-08
 
 # Run the Docker image as a container
-sudo docker run -d --rm --name gitlab-webapp-container -p 80:80 sbiswas232/gitlab-webapp:latest
+sudo docker run -d --name guard-webapp-container -p 80:80 sbiswas232/guard-webapp:adv1
+sudo docker run -d --name sbjapp-container -p 8080:8080 sbiswas232/springboot-javaapp:v21-08
